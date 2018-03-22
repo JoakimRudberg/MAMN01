04237d14cf3dc94eda6f7ebcac8f7ebc6469524d
JoakimRudberg <rudberg.joakim@gmail.com>
2018-03-22 16:49:03 +0100

    FIRST PUSH

---
+++ app/src/main/java/com/example/joakim/myapplication1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.joakim.myapplication1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void testCompass(View view) {
+        Intent intent = new Intent(this, testCompass.class);
+        startActivity(intent);
+    }
+
+    public void testAccelerometer(View view) {
+        Intent intent = new Intent(this, testAccelerometer.class);
+        startActivity(intent);
+    }
+
+}
